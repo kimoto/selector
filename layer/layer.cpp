acc8f9f723ae5c54c3765c6003bdc8f2fb9360cb
kimoto <sub+peerler@gmail.com>
2011-05-23 01:00:41 +0900

    *refactoring

--- layer/layer.cpp
+++ layer/layer.cpp
@@ -1,8 +1,8 @@
 ﻿#include <Windows.h>
 #include <WindowsX.h>
 #include <tchar.h>
+#include "Util.h"
 
-#define SafeDeleteObject(gdiobj) gdiobj != NULL && ::DeleteObject(gdiobj)
 #define CAPTURE_MIN_WIDTH 1
 #define CAPTURE_MIN_HEIGHT 1
 
@@ -24,23 +24,6 @@ ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK	WndProc2(HWND, UINT, WPARAM, LPARAM);
-
-void ShowLastError(void){
-	LPVOID lpMessageBuffer;
-
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL,
-		GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR) &lpMessageBuffer,
-		0,
-		NULL );
-
-	MessageBox(NULL, (LPCWSTR)lpMessageBuffer, TEXT("Error"), MB_OK);
-	LocalFree( lpMessageBuffer );
-}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -127,178 +110,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	UpdateWindow(hWnd2);
 
 	::MoveWindow(hWnd, rect.left, rect.top, rect.right, rect.bottom, TRUE);
-	return TRUE;
-}
-
-void TextFormatOut(HDC hdc, int x, int y, LPCTSTR format, ...)
-{
-	va_list arg;
-	va_start(arg, format);
-
-	TCHAR buffer[256];
-	::_vsnwprintf_s(buffer, 256, _TRUNCATE, format, arg);
-	::TextOut(hdc, x, y, buffer, lstrlen(buffer));
-	va_end(arg);
-}
-
-void ShadowTextFormatOut(HDC hdc, int x, int y, int w, COLORREF shadow, COLORREF color, LPCTSTR format, ...)
-{
-	va_list arg;
-	va_start(arg, format);
-
-	TCHAR buffer[256];
-	::SetBkMode(hdc, TRANSPARENT);
-	::_vsnwprintf_s(buffer, 256, _TRUNCATE, format, arg);
-
-	// 影の描画
-	::SetTextColor(hdc, shadow);
-	::TextOut(hdc, x + w, y + w, buffer, lstrlen(buffer));
-
-	// 本体の描画
-	::SetTextColor(hdc, color);
-	::TextOut(hdc, x, y, buffer, lstrlen(buffer));
-
-	va_end(arg);
-}
-
-#define BUFFER_SIZE 256
-void trace(LPCTSTR format, ...)
-{
-	va_list arg;
-	va_start(arg, format);
-
-	TCHAR buffer[BUFFER_SIZE];
-	::_vsnwprintf_s(buffer, BUFFER_SIZE, _TRUNCATE, format, arg);
-	::OutputDebugString(buffer);	
-	va_end(arg);
-}
-
-void RectangleNormalize(RECT *rect)
-{
-	// 常に左上基点の構造体に変換
-	if(rect->right - rect->left < 0){
-		// 左右逆
-		int tmp = rect->left;
-		rect->left = rect->right;
-		rect->right = tmp;
-	}
-	if(rect->bottom - rect->top < 0){
-		int tmp = rect->top;
-		rect->top = rect->bottom;
-		rect->bottom = tmp;
-	}
-}
-
-void StickRect(RECT *selected, RECT *target, int w_px, int h_px)
-{
-	// 左側
-	if(target->left <= selected->left && selected->left <= w_px){
-		selected->right = target->left + (selected->right - selected->left);
-		selected->left = target->left;
-	}
-	// 上側
-	if(target->top <= selected->top && selected->top <= h_px){
-		selected->bottom = target->top + (selected->bottom - selected->top);
-		selected->top = target->top;
-	}
-	// 下側
-	if(target->bottom - h_px <= selected->bottom && selected->bottom <= target->bottom){
-		selected->top = target->bottom - (selected->bottom - selected->top);
-		selected->bottom = target->bottom;
-	}
-	// 右側
-	if(target->right - w_px <= selected->right && selected->right <= target->right){
-		selected->left = target->right - (selected->right - selected->left);
-		selected->right = target->right;
-	}
-
-	// 上下反転したときの上側
-	if(selected->bottom < target->top + h_px){
-		selected->top = target->top + (selected->top - selected->bottom);
-		selected->bottom = target->top;
-	}
-	// 上下反転したときの下側
-	if(target->bottom - h_px <= selected->top){
-		selected->bottom = target->bottom - (selected->top - selected->bottom);
-		selected->top = target->bottom;
-	}
-	// 左右反転したときの左側
-	if(selected->right < target->left + w_px){
-		selected->left = selected->left - selected->right;
-		selected->right = target->left;
-	}
-	// 左右反転したときの右側
-	if(target->right - w_px < selected->left){
-		selected->right = target->right - (selected->left - selected->right);
-		selected->left = target->right;
-	}
-}
-
-// 指定されたウインドウ範囲から出られなくします
-void CorrectRect(RECT *selected, RECT *target)
-{
-	// 左側
-	if(selected->left < target->left){
-		selected->right = target->left + (selected->right - selected->left);
-		selected->left = target->left;
-	}
-	// 上側
-	if(selected->top < target->top){
-		selected->bottom = target->top + (selected->bottom - selected->top);
-		selected->top = target->top;
-	}
-	// 右側
-	// ...
-
-	// 右側(逆版)
-	if(selected->left > target->right){
-		int w = selected->left - selected->right;
-		selected->left = target->right;
-		selected->right = selected->left - w;
-	}
-
-	// 下側
-	/*
-	if(selected->bottom > target->bottom){
-	int h = selected->bottom - selected->top;
-	selected->bottom = target->bottom;
-	selected->top = selected->bottom - h;
-	}
-	*/
-
-	// 下側(逆版)
-	if(selected->top > target->bottom){
-		int h = selected->top - selected->bottom;
-		selected->top = target->bottom;
-		selected->bottom = selected->top - h;
-	}
-}
-
-// 指定されたウインドウを強調します
-BOOL HighlightWindow(HWND hWnd)
-{
-	HDC hdc = ::GetWindowDC(hWnd);
-	if(hdc == NULL){
-		return FALSE;
-	}
-
-	HPEN hPen = CreatePen(PS_SOLID, 5, RGB(255, 0, 0));
-	HBRUSH hBrush = (HBRUSH)::GetStockObject(HOLLOW_BRUSH);
-
-	HGDIOBJ hPrevPen = ::SelectObject(hdc, hPen);
-	HGDIOBJ hPrevBrush = ::SelectObject(hdc, hBrush);
-
-	RECT rect;
-	::GetWindowRect(hWnd, &rect);
-	::Rectangle(hdc, 0, 0, rect.right - rect.left, rect.bottom - rect.top);
-
-	::SelectObject(hdc, hPrevPen);
-	::SelectObject(hdc, hPrevBrush);
-
-	::DeleteObject(hPen);
-	::DeleteObject(hBrush);
-
-	::ReleaseDC(hWnd, hdc);
 	return TRUE;
 }
 
